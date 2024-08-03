edbb630eed245ee160791cf5230079a83abf9062
paulrogers <paulr909@proton.me>
2024-08-03 13:36:13 +0100

    Remove redundant components
    Deleted unused components such as Hero, Video, Testimonials, CTA, and FAQ from multiple pages to streamline the layout. Updated metadata in layout.tsx for better branding information.

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,28 +1,15 @@
 import { Container } from "@/components/Container"
-import { Hero } from "@/components/Hero"
 import { SectionTitle } from "@/components/SectionTitle"
 import { Benefits } from "@/components/Benefits"
-import { Video } from "@/components/Video"
-import { Testimonials } from "@/components/Testimonials"
 import { CTA } from "@/components/CTA"
 import { FAQ } from "@/components/FAQ"
 import { benefitOne, benefitTwo } from "@/components/data"
+
 export default function Features() {
   return (
     <Container>
-      <Hero />
-      <SectionTitle
-        preTitle="Next Web Benefits"
-        title=" Why should you use this landing page"
-      >
-        Next Web is a free landing page & marketing website template for
-        startups and indie projects. Its built with Next.js & TailwindCSS. And
-        its completely open-source.
-      </SectionTitle>
-
       <Benefits data={benefitOne} />
       <Benefits imgPos="right" data={benefitTwo} />
-
       <SectionTitle
         preTitle="Watch a video"
         title="Learn how to fullfil your needs"
@@ -31,9 +18,6 @@ export default function Features() {
         Analysts says a landing page with video has 3% more conversion rate. So,
         don&apos;t forget to add one. Just like this.
       </SectionTitle>
-
-      <Video videoId="fZ0D0cnR88E" />
-
       <SectionTitle
         preTitle="Testimonials"
         title="Here's what our customers said"
@@ -41,14 +25,6 @@ export default function Features() {
         Testimonials is a great way to increase the brand trust and awareness.
         Use this section to highlight your popular customers.
       </SectionTitle>
-
-      <Testimonials />
-
-      <SectionTitle preTitle="FAQ" title="Frequently Asked Questions">
-        Answer your customers possible questions here, it will increase the
-        conversion rate as well as support or chat requests.
-      </SectionTitle>
-
       <FAQ />
       <CTA />
     </Container>
